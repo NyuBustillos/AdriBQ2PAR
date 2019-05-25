09ad084695009965bb9023c178a390568013c0e4
Adri_bq <nyubustillos@gmail.com>
2019-05-25 10:08:04 -0400

    5to commit, Edicion del MainViewModels

--- AdriBQ2PAR/AdriBQ2PAR/AdriBQ2PAR/ViewModels/MainViewModel.cs
+++ AdriBQ2PAR/AdriBQ2PAR/AdriBQ2PAR/ViewModels/MainViewModel.cs
@@ -1,10 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
 
 namespace AdriBQ2PAR.ViewModels
 {
-    class MainViewModel
+    using Models;
+    using System;
+    using System.Collections.ObjectModel;
+
+    public class MainViewModel
     {
+
+        #region ViewModels
+        public LoginViewModel Login { get; set; }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            instance = this;
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance;
+
+        public static MainViewModel GetInstance()
+        {
+            if (instance == null)
+            {
+                return new MainViewModel();
+            }
+            return instance;
+        }
+        #endregion
+
     }
+
+
 }
