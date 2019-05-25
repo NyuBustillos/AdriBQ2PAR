357c12da1b0491eedd5b1d9a9a9b09a50184fdab
Adri_bq <nyubustillos@gmail.com>
2019-05-25 10:37:24 -0400

    Commit 8,Todo menos el API Porque no se como copiarlo

---
+++ AdriBQ2PAR/AdriBQ2PAR/AdriBQ2PAR/Services/API.cs
@@ -0,0 +1,14 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+
+namespace AdriBQ2PAR.Services
+{
+    
+
+}
