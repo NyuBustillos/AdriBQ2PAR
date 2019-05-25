f6488830bd635b23a437f62b168e8516c60755c2
Adri_bq <nyubustillos@gmail.com>
2019-05-25 09:47:50 -0400

    3er commit, Creacion login page

--- AdriBQ2PAR/AdriBQ2PAR/AdriBQ2PAR/App.xaml.cs
+++ AdriBQ2PAR/AdriBQ2PAR/AdriBQ2PAR/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdriBQ2PAR.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace AdriBQ2PAR
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
